f636eb279323d7c611299ccf47465e9005bac853
MusaOglanov <oglanovmusa2001@gmail.com>
2023-03-15 11:53:01 +0400

    Sql Connection

---
+++ Travel/Travel/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Travel.DAL
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+
+
+
+
+    }
+}
