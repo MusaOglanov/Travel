1bac73a2e44e092ef85a3ab1e5295edad1a849f9
MusaOglanov <oglanovmusa2001@gmail.com>
2023-03-17 14:55:07 +0400

    HotelTypesController və Create-ni yaratdım

---
+++ Travel/Travel/Controllers/HotelTypesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Travel.DAL;
+using Travel.Models;
+
+namespace Travel.Controllers
+{
+    public class HotelTypesController : Controller
+    {
+        private readonly AppDbContext _db;
+        public HotelTypesController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<HotelType> hotelTypes = await _db.HotelTypes.ToListAsync();
+            return View(hotelTypes);
+        }
+
+
+        public  IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HotelType hotelType)
+        {
+            bool IsExist = await _db.HotelTypes.AnyAsync(t => t.Name == hotelType.Name);
+            if (IsExist)
+            {
+                ModelState.AddModelError("Name", "This name already is exist");
+                return View();
+            }
+
+
+            await _db.HotelTypes.AddAsync(hotelType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Travel/Travel/Controllers/HotelsController.cs
+++ Travel/Travel/Controllers/HotelsController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AllUp3.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Travel.DAL;
 using Travel.Models;
@@ -10,9 +14,11 @@ namespace Travel.Controllers
     public class HotelsController : Controller
     {
         private readonly AppDbContext _db;
-        public HotelsController(AppDbContext db)
+        private readonly IWebHostEnvironment _env;
+        public HotelsController(AppDbContext db,IWebHostEnvironment env)
         {
             _db = db;
+            _env = env;
         }
         #region Index
         public async Task<IActionResult> Index()
@@ -26,8 +32,9 @@ namespace Travel.Controllers
         #endregion
 
         #region Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            ViewBag.HotelType = await _db.HotelTypes.ToListAsync();
             return View();
         }
         #region Get
@@ -35,10 +42,52 @@ namespace Travel.Controllers
         #endregion
 
         #region Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Hotel hotel, int[] hoteTypesId)
+        {
+            ViewBag.HotelType = await _db.HotelTypes.ToListAsync();
 
+            List<HotelCategory> hotelCategories = new List<HotelCategory>();
+
+            foreach (var hotelTypeId in hoteTypesId)
+            {
+                HotelCategory hotelCategory = new HotelCategory
+                {
+                    HotelTypeId = hotelTypeId,
+                };
+                hotelCategories.Add(hotelCategory);
+            }
+
+            hotel.HotelCategories = hotelCategories;
+
+            if (hotel.Photo == null)
+            {
+                ModelState.AddModelError("Photo", "Please slect image ");
+                return View();
+            }
+            if (!hotel.Photo.IsImage())
+            {
+                ModelState.AddModelError("Photo", "Please slect image file");
+                return View();
+            }
+            if (hotel.Photo.IsOlder2MB())
+            {
+                ModelState.AddModelError("Photo", "Max 2MB");
+                return View();
+            }
+            string folder = Path.Combine(_env.WebRootPath, "assets", "img");
+            hotel.Image = await hotel.Photo.SaveImageAsync(folder);
+
+            await _db.Hotels.AddAsync(hotel);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+
+        }
         #endregion
 
 
         #endregion
     }
-}
+    }
